b41d30d0ad6aa79be399babdfbf66515a890c6de
khnishou <kerdelyounes@gmail.com>
2024-01-10 17:31:41 +0100

    cpp07/ex01 finished

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,3 +1,43 @@
 
 #pragma once
 
+#define RED "\033[1;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define ORANGE "\033[38;5;202m\033[22m"
+#define GRAY "\033[1;30m"
+#define DEFAULT "\033[1;39m"
+
+#include <iostream>
+
+template <typename T, typename F>
+void iter(T* arr,int len, F func) {
+    int i;
+
+    i = 0;
+    while (i < len) {
+        func(arr[i]);
+        i++;
+    }
+}
+
+void printBlue(char c) {
+	std::cout << BLUE << c;
+}
+
+void printRed(char c) {
+	std::cout << RED << c;
+}
+
+void printGreen(char c) {
+	std::cout << GREEN << c;
+}
+
+void printYellow(char c) {
+	std::cout << YELLOW << c;
+}
+
+void printWhite(char c) {
+	std::cout << DEFAULT << c;
+}
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include "whatever.hpp"
+#include <string>
+#include "iter.hpp"
 
-int main( void ) {
+int main() {
+    std::string str = "random text";
 
-int a = 2;
-int b = 3;
-std::cout << std::endl << YELLOW << "~~~~~~~~~~~~~~~ int ~~~~~~~~~~~~~~~" << DEFAULT << std::endl;
-std::cout << "a = " << a << ", b = " << b << std::endl;
-::swap( a, b );
-std::cout << "a = " << a << ", b = " << b << std::endl;
-std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
-std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
-
-std::cout << std::endl << YELLOW << "~~~~~~~~~~~~~~~ string ~~~~~~~~~~~~~~~" << DEFAULT << std::endl;
-std::string c = "chaine1";
-std::string d = "chaine2";
-std::cout << "c = " << c << ", d = " << d << std::endl;
-::swap(c, d);
-std::cout << "c = " << c << ", d = " << d << std::endl;
-std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
-std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
-
-double e = 42.24;
-double f = 24.42;
-std::cout << std::endl << YELLOW << "~~~~~~~~~~~~~~~ double ~~~~~~~~~~~~~~~" << DEFAULT << std::endl;
-std::cout << "e = " << e << ", f = " << f << std::endl;
-::swap( e, f );
-std::cout << "e = " << e << ", f = " << f << std::endl;
-std::cout << "min(e, f) = " << ::min( e, f ) << std::endl;
-std::cout << "max(e, f) = " << ::max( e, f ) << std::endl;
-return 0;
+    iter(&str[0], str.length(), printBlue);
+	std::cout << std::endl;
+    iter(&str[0], str.length(), printRed);
+	std::cout << std::endl;
+    iter(&str[0], str.length(), printGreen);
+	std::cout << std::endl;
+    iter(&str[0], str.length(), printYellow);
+	std::cout << std::endl;
+    iter(&str[0], str.length(), printWhite);
+	return 0;
 }
