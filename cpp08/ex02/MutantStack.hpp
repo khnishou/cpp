54eabf2092aab0730256354bc5553e1d3df89d94
Younes Kerdel <ykerdel@3-B-9.42heilbronn.de>
2024-01-24 17:41:42 +0100

    cpp08/ex02 main needed

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -8,15 +8,19 @@
 #define GRAY "\033[1;30m"
 #define DEFAULT "\033[1;39m"
 
-#include <stack>
 #include <iterator>
+#include <stack>
 
-template <typename T>
-class MutantStack : public std::iterator<std::bidirectional_iterator_tag, T> {
-    public:
-        std::string iterator;
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+   public:
+	typedef typename Container::iterator iterator;
 
-        iterator begin() { return this->c.begin(); }
-	    iterator end() { return this->c.end(); }
+	MutantStack() {}
+	~MutantStack();
+	MutantStack(Container const& other) : std::stack<T, Container>::stack(other) {}
+	MutantStack& operator=(Container const& other) { std::stack<T, Container>::operator=(other); }
 
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
 };
