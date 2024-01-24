b572a1acd80406c20966e203b5e0b4efb45b1dd5
khnishou <kerdelyounes@gmail.com>
2024-01-24 03:40:36 +0100

    cpp08/ex01 added 10000 test case

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,12 +1,12 @@
 #include "Span.hpp"
 #include <iostream>
 
-void testSpan() {
+void testSpan1() {
     try
     {
         Span span(5);
 
-        std::cout << BLUE << "Test Case 1: Adding numbers and calculating spans" << DEFAULT << std::endl;
+        std::cout << BLUE << "Test 1: Adding numbers and calculating spans" << DEFAULT << std::endl;
 
         span.addNumber(5);
         span.addNumber(10);
@@ -20,16 +20,57 @@ void testSpan() {
         std::cout << "Shortest Span: " << GREEN << shortest << DEFAULT << std::endl;
         std::cout << "Longest Span: " << GREEN << longest << DEFAULT << std::endl;
 
-        std::cout << BLUE << "Test Case 2: Trying to add more than allowed numbers" << DEFAULT << std::endl;
-
-        span.addNumber(1);
     }
     catch (const std::exception& e) {
         std::cerr << RED << "Error: " << e.what() << DEFAULT << std::endl;
     }
 }
 
+void testSpan2() {
+    try
+    {
+        Span span(2);
+
+        std::cout << BLUE << "Test 2: Trying to add more than allowed numbers" << DEFAULT << std::endl;
+
+        span.addNumber(5);
+        span.addNumber(10);
+        span.addNumber(3);
+
+        unsigned int shortest = span.shortestSpan();
+        unsigned int longest = span.longestSpan();
+
+        std::cout << "Shortest Span: " << GREEN << shortest << DEFAULT << std::endl;
+        std::cout << "Longest Span: " << GREEN << longest << DEFAULT << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << RED << "Error: " << e.what() << DEFAULT << std::endl;
+    }
+}
+
+void testSpan3() {
+    try {
+        Span span(10000);
+
+        std::cout << BLUE << "Test Case 3: Adding 10,000 numbers and calculating spans" << DEFAULT << std::endl;
+
+        for (int i = 1; i <= 10000; ++i) {
+            span.addNumber(i);
+        }
+
+        unsigned int shortest = span.shortestSpan();
+        unsigned int longest = span.longestSpan();
+
+        std::cout << "Shortest Span: " << GREEN << shortest << DEFAULT << std::endl;
+        std::cout << "Longest Span: " << GREEN << longest << DEFAULT << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << RED << "Error: " << e.what() << DEFAULT << std::endl;
+    }
+}
+
 int main() {
-    testSpan();
+    testSpan1();
+    testSpan2();
+    testSpan3();
     return 0;
 }
